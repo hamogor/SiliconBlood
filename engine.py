125392a18e8de6fb1b91f30a2f50bcd2df45c283
oref <ferovax@gmail.com>
2019-06-19 20:01:32 +0100

    added game_map, tiles, render_functions, entities and drawn a small map

--- engine.py
+++ engine.py
@@ -1,36 +1,45 @@
 import tcod as libtcod
 
+from entity import Entity
 from input_handlers import handle_keys
+from map_objects.game_map import GameMap
+from render_functions import clear_all, render_all
 
 
 def main():
-    # Screen size
     screen_width = 80
     screen_height = 50
+    map_width = 80
+    map_height = 45
 
-    # Set player position to middle of screen
-    player_x = int(screen_width / 2)
-    player_y = int(screen_height / 2)
+    colors = {
+        'dark_wall': libtcod.Color(0, 0, 100),
+        'dark_ground': libtcod.Color(50, 50, 150)
+    }
+
+    player = Entity(int(screen_width / 2), int(screen_height / 2), '@', libtcod.white)
+    npc = Entity(int(screen_width / 2 - 5), int(screen_height / 2), '@', libtcod.yellow)
+    entities = [npc, player]
 
     libtcod.console_set_custom_font('arial10x10.png', libtcod.FONT_TYPE_GREYSCALE | libtcod.FONT_LAYOUT_TCOD)
 
-    libtcod.console_init_root(screen_width, screen_height, 'Escape The Deepwoods', False)
+    libtcod.console_init_root(screen_width, screen_height, 'libtcod tutorial revised', False)
 
     con = libtcod.console_new(screen_width, screen_height)
+
+    game_map = GameMap(map_width, map_height)
 
     key = libtcod.Key()
     mouse = libtcod.Mouse()
 
-    # Game loop
     while not libtcod.console_is_window_closed():
         libtcod.sys_check_for_event(libtcod.EVENT_KEY_PRESS, key, mouse)
 
-        libtcod.console_set_default_foreground(con, libtcod.white)  # Set '@' character to white
-        libtcod.console_put_char(con, player_x, player_y, '@', libtcod.BKGND_NONE)  # Put our player in middle of screen
-        libtcod.console_blit(con, 0, 0, screen_width, screen_height, 0, 0, 0)
-        libtcod.console_flush()  # Draw
+        render_all(con, entities, game_map, screen_width, screen_height, colors)
 
-        libtcod.console_put_char(con, player_x, player_y, ' ', libtcod.BKGND_NONE)
+        libtcod.console_flush()
+
+        clear_all(con, entities)
 
         action = handle_keys(key)
 
@@ -40,8 +49,9 @@ def main():
 
         if move:
             dx, dy = move
-            player_x += dx
-            player_y += dy
+
+            if not game_map.is_blocked(player.x + dx, player.y + dy):
+                player.move(dx, dy)
 
         if exit:
             return True
@@ -51,4 +61,4 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+     main()
