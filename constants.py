c31bd37a5b818b3228bb89cfa1421e7343c6376b
oref <ferovax@gmail.com>
2019-06-26 23:29:24 +0100

    map drawing and migration to pygame

--- constants.py
+++ constants.py
@@ -6,6 +6,12 @@ pygame.init()
 # Game sizes
 GAME_WIDTH = 800
 GAME_HEIGHT = 600
+CELL_WIDTH = 32
+CELL_HEIGHT = 32
+
+# Map vars
+MAP_WIDTH = 30
+MAP_HEIGHT = 30
 
 # Color definitions
 COLOR_BLACK = (0, 0, 0)
@@ -17,3 +23,5 @@ COLOR_DEFAULT_BG = COLOR_GREY
 
 # Sprites
 S_PLAYER = pygame.image.load("data/player.png")
+S_WALL = pygame.image.load("data/wall.png")
+S_FLOOR = pygame.image.load("data/floor.png")
--- main.py
+++ main.py
@@ -5,6 +5,20 @@ import pygame
 import constants
 
 
+class StrucTile:
+    def __init__(self, block_path):
+        self.block_path = block_path
+
+
+def map_create():
+    new_map = [[StrucTile(False) for y in range(0, constants.MAP_WIDTH)] for x in range(0, constants.MAP_HEIGHT)]
+
+    new_map[10][10].block_path = True
+    new_map[10][15].block_path = True
+
+    return new_map
+
+
 def game_main_loop():
 
     game_quit = False
@@ -28,11 +42,13 @@ def game_main_loop():
 
 def game_initialize():
 
-    global SURFACE_MAIN
+    global SURFACE_MAIN, GAME_MAP
 
     pygame.init()
 
     SURFACE_MAIN = pygame.display.set_mode((constants.GAME_WIDTH, constants.GAME_HEIGHT))
+
+    GAME_MAP = map_create()
 
 
 def draw_game():
@@ -40,10 +56,20 @@ def draw_game():
 
     SURFACE_MAIN.fill(constants.COLOR_DEFAULT_BG)
 
+    draw_map(GAME_MAP)
+
     SURFACE_MAIN.blit(constants.S_PLAYER, (constants.GAME_WIDTH / 2, constants.GAME_HEIGHT / 2))
 
     pygame.display.flip()
 
+
+def draw_map(map_to_draw):
+    for x in range(0, constants.MAP_WIDTH):
+        for y in range(0, constants.MAP_HEIGHT):
+            if map_to_draw[x][y].block_path:
+                SURFACE_MAIN.blit(constants.S_WALL, (x*constants.CELL_WIDTH, y*constants.CELL_WIDTH))
+            else:
+                SURFACE_MAIN.blit(constants.S_FLOOR, (x * constants.CELL_WIDTH, y * constants.CELL_WIDTH))
 
 
 if __name__ == '__main__':
