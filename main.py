a79c0fbade5f35e2b22472c1a304fc7d2f93e880
oref <ferovax@gmail.com>
2019-06-27 19:53:13 +0100

    bugged movement

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # 3rd party modules
-import tcod as libtcod
 import pygame
 
+# game files
 import constants
 
 
@@ -10,8 +10,27 @@ class StrucTile:
         self.block_path = block_path
 
 
+class ObjActor:
+    """Our basic actor object."""
+
+    def __init__(self, x, y, sprite):
+        self.x = x
+        self.y = y
+        self.sprite = sprite
+
+    def draw(self):
+        SURFACE_MAIN.blit(self.sprite, (self.x*constants.CELL_WIDTH,
+                                        self.y*constants.CELL_HEIGHT))
+
+    def move(self, dx, dy):
+        print("called")
+        if GAME_MAP[self.x + dx][self.y + dy].block_path:
+            self.x += dx
+            self.y += dy
+
+
 def map_create():
-    new_map = [[StrucTile(False) for y in range(0, constants.MAP_WIDTH)] for x in range(0, constants.MAP_HEIGHT)]
+    new_map = [[StrucTile(False) for y in range(0, constants.MAP_HEIGHT)] for x in range(0, constants.MAP_WIDTH)]
 
     new_map[10][10].block_path = True
     new_map[10][15].block_path = True
@@ -19,57 +38,78 @@ def map_create():
     return new_map
 
 
-def game_main_loop():
+def draw_game():
+    global SURFACE_MAIN
 
+    # clear the surface
+    SURFACE_MAIN.fill(constants.COLOR_DEFAULT_BG)
+
+    # draw the map
+    draw_map(GAME_MAP)
+
+    # draw the character
+    PLAYER.draw()
+
+    # update the display
+    pygame.display.flip()
+
+
+def draw_map(map_to_draw):
+
+    for x in range(0, constants.MAP_WIDTH):
+        for y in range(0, constants.MAP_HEIGHT):
+            if map_to_draw[x][y].block_path: # if True there is a wall here
+                # draw wall
+                SURFACE_MAIN.blit(constants.S_WALL, (x*constants.CELL_WIDTH, y*constants.CELL_HEIGHT))
+            else:
+                # draw floor
+                SURFACE_MAIN.blit(constants.S_FLOOR, (x*constants.CELL_WIDTH, y*constants.CELL_HEIGHT))
+
+
+def game_main_loop():
+    """In this function we loop the main game."""
     game_quit = False
 
     while not game_quit:
 
-        # Get player input
+        # get player input
         events_list = pygame.event.get()
 
-        # Process input
-        for event in events_list:
-            if event.type is pygame.QUIT:
+        # process input
+        for event in events_list:  # loop through all events that have happened
+            if event.type == pygame.QUIT:  # QUIT attribute - someone closed window
                 game_quit = True
 
-        # Draw the game
-        draw_game()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_UP:
+                    PLAYER.move(0, -1)
+                if event.key == pygame.K_DOWN:
+                    PLAYER.move(0, 1)
+                if event.key == pygame.K_LEFT:
+                    PLAYER.move(-1, 0)
+                if event.key == pygame.K_RIGHT:
+                    PLAYER.move(1, 0)
+        # draw the game
+            draw_game()
 
     pygame.quit()
     exit()
 
 
 def game_initialize():
+    """This function initializes the main window, and pygame"""
 
-    global SURFACE_MAIN, GAME_MAP
-
+    global SURFACE_MAIN, GAME_MAP, PLAYER
+    # initialize pygame
     pygame.init()
 
+    # set sufrace dimensions
     SURFACE_MAIN = pygame.display.set_mode((constants.GAME_WIDTH, constants.GAME_HEIGHT))
+    # Ideally the surface should be resizable -- we are going to skip this for now
 
-    GAME_MAP = map_create()
+    GAME_MAP = map_create()  # Create the game map. Fills the 2D array with values.
 
-
-def draw_game():
-    global SURFACE_MAIN
-
-    SURFACE_MAIN.fill(constants.COLOR_DEFAULT_BG)
-
-    draw_map(GAME_MAP)
-
-    SURFACE_MAIN.blit(constants.S_PLAYER, (constants.GAME_WIDTH / 2, constants.GAME_HEIGHT / 2))
-
-    pygame.display.flip()
-
-
-def draw_map(map_to_draw):
-    for x in range(0, constants.MAP_WIDTH):
-        for y in range(0, constants.MAP_HEIGHT):
-            if map_to_draw[x][y].block_path:
-                SURFACE_MAIN.blit(constants.S_WALL, (x*constants.CELL_WIDTH, y*constants.CELL_WIDTH))
-            else:
-                SURFACE_MAIN.blit(constants.S_FLOOR, (x * constants.CELL_WIDTH, y * constants.CELL_WIDTH))
+    PLAYER = ObjActor(int(constants.MAP_WIDTH / 2), int(constants.MAP_HEIGHT / 2), constants.S_PLAYER)
 
 
 if __name__ == '__main__':
