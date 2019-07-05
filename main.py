ffd9f9e11729ef683b770c39d771f3b474f5d9ba
oref <ferovax@gmail.com>
2019-07-05 12:03:49 +0100

    movement

--- main.py
+++ main.py
@@ -4,27 +4,23 @@ import pygame
 
 
 class Main:
-    def run(self):
+    def __init__(self):
         pygame.init()
-        surface = pygame.display.set_mode((WIDTH, HEIGHT))
+        self.surface = pygame.display.set_mode((WIDTH, HEIGHT))
+        pygame.display.set_caption(TITLE)
         player_x, player_y = (0, 0)
 
-        for x in range(0, WIDTH, TILESIZE):
-            pygame.draw.line(surface, LIGHTGREY, (x, 0), (x, HEIGHT))
-        for y in range(0, HEIGHT, TILESIZE):
-            pygame.draw.line(surface, LIGHTGREY, (0, y), (WIDTH, y))
-
         while True:
-            events_list = pygame.event.get()
+            events_list = pygame.event.get()#
+            self.surface.fill(BGCOLOR)
+            self.draw_map()
 
-            # process input3
             for event in events_list:
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
 
                 if event.type == pygame.KEYDOWN:
-
                     if event.key in MOVE_N:
                         player_y -= 1
                     elif event.key in MOVE_S:
@@ -34,7 +30,18 @@ class Main:
                     elif event.key in MOVE_E:
                         player_x += 1
 
+                    print(player_x)
+                    print(player_y)
+
+            self.surface.blit(S_PLAYER, (player_x * TILESIZE, player_y * TILESIZE))
+
             pygame.display.flip()
+
+    def draw_map(self):
+        for x in range(0, WIDTH, TILESIZE):
+            pygame.draw.line(self.surface, LIGHTGREY, (x, 0), (x, HEIGHT))
+        for y in range(0, HEIGHT, TILESIZE):
+            pygame.draw.line(self.surface, LIGHTGREY, (0, y), (WIDTH, y))
 
 
 if __name__ == '__main__':
