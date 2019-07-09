cf419c2c96d9e527388d0e98cfdb26f5ecffdc7e
oref <ferovax@gmail.com>
2019-07-09 01:58:34 +0100

    implemented collision component

--- ecs/collision/collision.py
+++ ecs/collision/collision.py
@@ -10,19 +10,17 @@ class CollisionSystem:
 
     def update(self, entities):
         for e in entities:
-            c_collision = e.get(CollisionComponent)
-            if not self.game_map[c_collision.x][c_collision.y].block_path:
-                e.get(DisplayComponent).x = c_collision.x * TILESIZE
-                e.get(DisplayComponent).y = c_collision.y * TILESIZE
+            cur_x = e.get(DisplayComponent).x
+            cur_y = e.get(DisplayComponent).y
+
+            d_x = e.get(CollisionComponent).x
+            d_y = e.get(CollisionComponent).y
+
+            if not self.game_map[d_x][d_y].block_path:
+                e.get(DisplayComponent).x = d_x * TILESIZE
+                e.get(DisplayComponent).y = d_y * TILESIZE
             else:
-                pass
-# 320 display y
-# 288 display x
-# 9 collision x
-# 10 collision y
-#
-#
-# 320
-# 288
-# 11
-# 10
+                e.get(DisplayComponent).x = cur_x
+                e.get(DisplayComponent).y = cur_y
+                e.get(CollisionComponent).x = int(cur_x / TILESIZE)
+                e.get(CollisionComponent).y = int(cur_y / TILESIZE)
