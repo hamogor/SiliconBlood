b19072bb1b370a6d0641591f39af81ce45899a32
oref <orefdev@gmail.com>
2019-07-19 11:34:26 +0100

    fixes to camera

--- ecs/display/display.py
+++ ecs/display/display.py
@@ -20,22 +20,23 @@ class DisplaySystem:
             if e.get(FovComponent).fov_recalculate:
                 for x in range(0, CAM_WIDTH):
                     for y in range(0, CAM_HEIGHT):
-                        k, j = self.camera.apply(x, y)
+                        cam_x, cam_y = self.camera.apply(x, y)
                         wall = self.map.tiles[x][y].block_path
                         visible = tcod.map_is_in_fov(e.get(FovComponent).fov_map, x, y)
+
                         if visible:
                             if wall:
-                                print("XY {} {}".format(x, y))
-                                self._root_display.blit(S_WALL, (k * TILESIZE, j * TILESIZE))
-                                print("KJ {} {}".format(k, j))
+                                self._root_display.blit(S_WALL, (cam_x * TILESIZE, cam_x * TILESIZE))
                             else:
-                                self._root_display.blit(S_FLOOR, (k * TILESIZE, j * TILESIZE))
-                            self.map.tiles[k][j].explored = True
+                                self._root_display.blit(S_FLOOR, (cam_x * TILESIZE, cam_y * TILESIZE))
+
+                            self.map.tiles[x][y].explored = True
+
                         elif self.map.tiles[x][y].explored:
                             if self.map.tiles[x][y].block_path:
-                                self._root_display.blit(S_DWALL, (k * TILESIZE, j * TILESIZE))
+                                self._root_display.blit(S_DWALL, (cam_x * TILESIZE, cam_y * TILESIZE))
                             else:
-                                self._root_display.blit(S_DFLOOR, (k * TILESIZE, j * TILESIZE))
+                                self._root_display.blit(S_DFLOOR, (cam_x * TILESIZE, cam_y * TILESIZE))
 
             self._root_display.blit(S_PLAYER, (15 * TILESIZE, 10 * TILESIZE))
         pygame.display.flip()
@@ -53,11 +54,9 @@ class Camera:
     def apply(self, x, y):
         x = x + self.x
         y = y + self.y
-        print("Apply: {} {}".format(x, y))
         return x, y
 
     def update(self, player):
-        x = -player.get(MovementComponent).x + int(self.width / 2)
-        y = -player.get(MovementComponent).y + int(self.height / 2)
-        #print("Update: {} {}".format(x, y))
+        x = player.get(MovementComponent).x - int(self.width / 2)
+        y = player.get(MovementComponent).y - int(self.height / 2)
         self.x, self.y = x, y
