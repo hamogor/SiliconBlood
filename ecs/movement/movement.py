289eccf9583f5eed18ea9e9a5a6853be43144de9
oref <ferovax@gmail.com>
2019-07-20 02:30:37 +0100

    fixed top/left bounds

--- ecs/movement/movement.py
+++ ecs/movement/movement.py
@@ -16,6 +16,10 @@ class MovementSystem:
     def move(self, e):
         try:
             wall = self.game_map[e.get(MovementComponent).x][e.get(MovementComponent).y].block_path
+            if e.get(MovementComponent).y < 0:
+                wall = True
+            if e.get(MovementComponent).x < 0:
+                wall = True
         except IndexError:
             wall = True
         if not wall:
