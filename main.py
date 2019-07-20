68dd3b7267940d9fe4e361831c7e66e9fc14ca72
oref <ferovax@gmail.com>
2019-07-20 18:50:07 +0100

    refactored main

--- main.py
+++ main.py
@@ -1,58 +1,62 @@
-from constants import *
-from ecs.display.display import DisplaySystem
-from ecs.human.player import Player
-from ecs.input.keyboard import KeyboardInputSystem
-from ecs.movement.movement import MovementSystem
-from ecs.fov.fov import FovSystem
+import pygame
+from settings import *
 
 from ecs.container import Container
-import pygame
+from ecs.display.display import DisplaySystem
+from ecs.camera.camera import CameraSystem
+from ecs.fov.fov import FovSystem
+from ecs.keyboard.keyboard import KeyboardSystem
+from ecs.movement.movement import MovementSystem
+from ecs.level.level import LevelSystem
+
+from actors.player import Player
 
 
-class Main:
+class SiliconBlood:
     def __init__(self):
         pygame.init()
         pygame.display.set_caption(TITLE)
-        pygame.display.set_icon(S_PLAYER)
+        pygame.display.set_icon(S_ENEMY)
         self.game_over = False
 
+        self.level_system = LevelSystem()
+
         self.player = Player()
-        self.display_system = DisplaySystem()
-        self.fov_system = FovSystem(self.display_system.map.tiles)
-        self.keyboard_input_system = KeyboardInputSystem()
-        self.movement_system = MovementSystem(self.display_system.map.tiles)
+        self.keyboard_system = KeyboardSystem()
+        self.display_system = DisplaySystem(self.level_system)
+        self.camera_system = CameraSystem(self.level_system)
+        self.fov_system = FovSystem(self.level_system)
+        self.movement_system = MovementSystem(self.level_system)
 
         self.container = Container()
-        self.container.add_system(self.keyboard_input_system)
-        self.container.add_system(self.movement_system)
-        self.container.add_system(self.fov_system)
+        self.container.add_system(self.keyboard_system)
+        self.container.add_system(self.level_system)
         self.container.add_system(self.display_system)
-        self.container.add_entity(self.player)
+        self.container.add_system(self.camera_system)
+        self.container.add_system(self.fov_system)
+        self.container.add_system(self.movement_system)
 
     def game_loop(self):
         self.container.update()
 
         while not self.game_over:
             self.check_for_game_over()
-            self.keyboard_input_system.update(self.container.entities)
-            if self.keyboard_input_system.keys_pressed:
+            self.keyboard_system.update(self.container.entities)
+            if self.keyboard_system.keys_pressed:
                 self.movement_system.update(self.container.entities)
                 self.fov_system.update(self.container.entities)
                 self.display_system.update(self.container.entities)
 
-
-
     def check_for_game_over(self):
-        if self.keyboard_input_system.get_all_keys_pressed():
-            keys_pressed = [e for e in self.keyboard_input_system.get_all_keys_pressed() if e == QUIT]
+        if self.keyboard_system.get_keys():
+            keys_pressed = [e for e in self.keyboard_system.get_keys() if e == QUIT]
             if keys_pressed:
                 self.game_over = True
 
-    def check_if_time_passed(self):
-        key_pressed = self.keyboard_input_system.get_all_keys_pressed()
+        def check_if_time_passed(self):
+            key_pressed = self.keyboard_input_system.get_all_keys_pressed()
 
-        return key_pressed
-
+            return key_pressed
 
 if __name__ == '__main__':
-    Main().game_loop()
+    SiliconBlood().game_loop()
