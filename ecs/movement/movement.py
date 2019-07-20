301f6941ff53c8bc85eb898376d8fbdcbb62e2a2
oref <ferovax@gmail.com>
2019-07-20 02:16:02 +0100

    Camera almost working, no right/bottom bounds and collision still off

--- ecs/movement/movement.py
+++ ecs/movement/movement.py
@@ -1,5 +1,5 @@
 from ecs.movement.movement_component import MovementComponent
-from ecs.display.display_component import DisplayComponent
+from ecs.display.display import CameraComponent
 from ecs.fov.fov_component import FovComponent
 from constants import *
 import pysnooper
@@ -11,17 +11,14 @@ class MovementSystem:
 
     def update(self, entities):
         for e in entities:
-            cur_x = int(e.get(MovementComponent).cur_x)
-            cur_y = int(e.get(MovementComponent).cur_y)
+            self.move(e)
 
-            d_x = e.get(MovementComponent).x
-            d_y = e.get(MovementComponent).y
-            if not self.game_map[d_x][d_y].block_path:
-                e.get(FovComponent).fov_recalculate = True
-                e.get(MovementComponent).x = d_x
-                e.get(MovementComponent).y = d_y
-                e.get(MovementComponent).cur_x = d_x
-                e.get(MovementComponent).cur_y = d_y
-            else:
-                e.get(MovementComponent).x = cur_x
-                e.get(MovementComponent).y = cur_y
+    def move(self, e):
+        if not self.game_map[e.get(MovementComponent).x][e.get(MovementComponent).y].block_path:
+            e.get(FovComponent).fov_recalculate = True
+            e.get(MovementComponent).x = e.get(MovementComponent).x
+            e.get(MovementComponent).y = e.get(MovementComponent).y
+        else:
+            e.get(MovementComponent).x = e.get(MovementComponent).cur_x
+            e.get(MovementComponent).y = e.get(MovementComponent).cur_y
+
